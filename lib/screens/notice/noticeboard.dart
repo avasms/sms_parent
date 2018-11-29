e3c5ff18c85b47772a143c10555ae983e47b41ce
AVASMS <avasmsteam@gmail.com>
2018-11-29 13:49:23 +0630

    APO

--- lib/screens/notice/noticeboard.dart
+++ lib/screens/notice/noticeboard.dart
@@ -1,5 +1,5 @@
 import 'dart:io';
-
+import 'dart:async';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:cached_network_image/cached_network_image.dart';
