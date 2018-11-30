183c67bff8f17508fb1b9da384ba079170d292f6
AVA SMS <avasmsteam@gmail.com>
2018-11-30 16:59:17 +0630

    apo

--- lib/main.dart
+++ lib/main.dart
@@ -36,20 +36,7 @@ void main() async {
     await LocalStorage.save(Config.USER_RELATED_ID, result.parentId);
     _isLogin = true;
   }
-  var settings = {
-    OSiOSSettings.autoPrompt: false,
-    OSiOSSettings.promptBeforeOpeningPushUrl: true
-  };
-//will delay initialization of the SDK
-//make sure to call before init()
-  await OneSignal.shared
-      .init("cecb6df8-3335-4db1-a26b-ed4b7f1c9ae3", iOSSettings: settings);
-// the SDK will now initialize
-  await OneSignal.shared.consentGranted(true);
-//
-var status = await OneSignal.shared.getPermissionSubscriptionState();
-    print(status.subscriptionStatus.userId);
-
+  
   runApp(new MyApp(loginStatus: _isLogin));
 }
 
@@ -71,191 +58,209 @@ class _MyAppState extends State<MyApp> {
     application.onLocaleChanged = onLocaleChange;
     // Message
     super.initState();
-
-    _initMessageNotiState();
-  }
-
-  Future<void> _initMessageNotiState() async {
-    if (!mounted) return;
-    OneSignal.shared
-        .setInFocusDisplayType(OSNotificationDisplayType.notification);
-    OneSignal.shared.setNotificationReceivedHandler((notification) {
-      this.setState(() {
-        print(
-            "Received notification: \n${notification.jsonRepresentation().replaceAll("\\n", "\n")}");
-      });
-    });
-
-    OneSignal.shared
-        .setNotificationOpenedHandler((OSNotificationOpenedResult result) {
-      this.setState(() {
-        print(
-            "Opened notification: \n${result.notification.jsonRepresentation().replaceAll("\\n", "\n")}");
-      });
-    });
-
-    OneSignal.shared
-        .setSubscriptionObserver((OSSubscriptionStateChanges changes) {
-      print("SUBSCRIPTION STATE CHANGED: ${changes.jsonRepresentation()}");
-    });
-
-    OneSignal.shared.setPermissionObserver((OSPermissionStateChanges changes) {
-      print("PERMISSION STATE CHANGED: ${changes.jsonRepresentation()}");
-    });
-
-    OneSignal.shared.setEmailSubscriptionObserver(
-        (OSEmailSubscriptionStateChanges changes) {
-      print("EMAIL SUBSCRIPTION STATE CHANGED ${changes.jsonRepresentation()}");
-    });
+    _getOneSignalData();
+        _initMessageNotiState();
+      }
     
-  }
+      Future<void> _initMessageNotiState() async {
+        if (!mounted) return;
+        OneSignal.shared
+            .setInFocusDisplayType(OSNotificationDisplayType.notification);
+        OneSignal.shared.setNotificationReceivedHandler((notification) {
+          this.setState(() {
+            print(
+                "Received notification: \n${notification.jsonRepresentation().replaceAll("\\n", "\n")}");
+          });
+        });
+    
+        OneSignal.shared
+            .setNotificationOpenedHandler((OSNotificationOpenedResult result) {
+          this.setState(() {
+            print(
+                "Opened notification: \n${result.notification.jsonRepresentation().replaceAll("\\n", "\n")}");
+          });
+        });
+    
+        OneSignal.shared
+            .setSubscriptionObserver((OSSubscriptionStateChanges changes) {
+          print("SUBSCRIPTION STATE CHANGED: ${changes.jsonRepresentation()}");
+        });
+    
+        OneSignal.shared.setPermissionObserver((OSPermissionStateChanges changes) {
+          print("PERMISSION STATE CHANGED: ${changes.jsonRepresentation()}");
+        });
+    
+        OneSignal.shared.setEmailSubscriptionObserver(
+            (OSEmailSubscriptionStateChanges changes) {
+          print("EMAIL SUBSCRIPTION STATE CHANGED ${changes.jsonRepresentation()}");
+        });
+        
+      }
+    
+      @override
+      void dispose() {
+        super.dispose();
+      }
+    
+      void onLocaleChange(Locale locale) {
+        setState(() {
+          _newLocaleDelegate = AppTranslationsDelegate(newLocale: locale);
+        });
+      }
+    
+      // This widget is the root of your application.
+      @override
+      Widget build(BuildContext context) {
+        // Create the router.
+        Router router = new Router();
+        // Define our Home page.
+        router.define('home', handler: new Handler(
+            handlerFunc: (BuildContext context, Map<String, dynamic> params) {
+          return new HomeScreen();
+        }));
+        // Define our Login page.
+        router.define('login', handler: new Handler(
+            handlerFunc: (BuildContext context, Map<String, dynamic> params) {
+          return new LoginScreen();
+        }));
+    
+        // Define our Dormantory page.
+        router.define('dormitory', handler: new Handler(
+            handlerFunc: (BuildContext context, Map<String, dynamic> params) {
+          return new DormitoryListData();
+        }));
+    
+        // Define our Dormantory page.
+        router.define('ferry', handler: new Handler(
+            handlerFunc: (BuildContext context, Map<String, dynamic> params) {
+          return new TransportListData();
+        }));
+    
+        // Define our Student page.
+        router.define('student', handler: new Handler(
+            handlerFunc: (BuildContext context, Map<String, dynamic> params) {
+          String _parentId = params["parentId"]?.first;
+          String _screenType = params["screenType"]?.first;
+          String _userId = params["userId"]?.first;
+          return new StudentListData(
+              parentId: _parentId, screenType: _screenType, userId: _userId);
+        }));
+    
+        // Define our Grade page.
+        router.define('grade', handler: new Handler(
+            handlerFunc: (BuildContext context, Map<String, dynamic> params) {
+          String _studentId = params["studentId"]?.first;
+          return new ExamGradeListData(
+            studentId: _studentId,
+          );
+        }));
+    
+        // Define our Exam page.
+        router.define('exam', handler: new Handler(
+            handlerFunc: (BuildContext context, Map<String, dynamic> params) {
+          String _classId = params["classId"]?.first;
+          return new ExamScreen(
+            classId: _classId,
+          );
+        }));
+    
+        // Define our Time Table page.
+        router.define('timetable', handler: new Handler(
+            handlerFunc: (BuildContext context, Map<String, dynamic> params) {
+          String _sectionId = params["sectionId"]?.first;
+          return new TimeTable(
+            sectionId: _sectionId,
+          );
+        }));
+    
+        // Define our Time Table page.
+        router.define('leave', handler: new Handler(
+            handlerFunc: (BuildContext context, Map<String, dynamic> params) {
+          String _sid = params["studentId"]?.first;
+          String _puid = params["userId"]?.first;
+          String _sname = params["studentName"]?.first;
+          return new LeaveScreen(
+              userId: _puid, studentId: _sid, studentName: _sname);
+        }));
+        //Define our Setting Page.
+        router.define('setting', handler: new Handler(
+            handlerFunc: (BuildContext context, Map<String, dynamic> params) {
+          String _userid = params["userId"]?.first;
+          return new Setting(
+            userId: _userid,
+          );
+        }));
+    
+        //Define our Notice Page.
+        router.define('notice', handler: new Handler(
+            handlerFunc: (BuildContext context, Map<String, dynamic> params) {
+          return new NoticeBoardScreen();
+        }));
+    
+        //Define our Notice Page.
+        router.define('message', handler: new Handler(
+            handlerFunc: (BuildContext context, Map<String, dynamic> params) {
+          String _userid = params["userId"]?.first;
+          return new MessageScreen(userId: _userid);
+        }));
+    
+        //Define our AboutSchool Page.
+        router.define('aboutSchool', handler: new Handler(
+            handlerFunc: (BuildContext context, Map<String, dynamic> params) {
+          return new AboutScreen();
+        }));
+    
+        //Define our Student Detail Page.
+        router.define('studentDetail', handler: new Handler(
+            handlerFunc: (BuildContext context, Map<String, dynamic> params) {
+          String _sid = params["studentId"]?.first;
+          String _sname = params["studentName"]?.first;
+          return new StudentDetailScreen(
+            studentId: _sid,
+            studentName: _sname,
+          );
+        }));
+    
+        // Defind Router
+        Application.router = router;
+    
+        return new MaterialApp(
+          title: 'AVASMS',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(primarySwatch: Colors.deepPurple, fontFamily: 'Zawgyi'),
+          onGenerateRoute: Application.router.generator,
+          home: widget.loginStatus ? new HomeScreen() : new LoginScreen(),
+          localizationsDelegates: [
+            _newLocaleDelegate,
+            const AppTranslationsDelegate(),
+            //provides localised strings
+            GlobalMaterialLocalizations.delegate,
+            //provides RTL support
+            GlobalWidgetsLocalizations.delegate,
+          ],
+          supportedLocales: application.supportedLocales(),
+        );
+      }
+    
+      Future<String> getLocalStorageData() async {
+        return await LocalStorage.get(Config.USER_RELATED_ID);
+      }
+    
+      void _getOneSignalData() async{
 
-  @override
-  void dispose() {
-    super.dispose();
-  }
+        var settings = {
+    OSiOSSettings.autoPrompt: false,
+    OSiOSSettings.promptBeforeOpeningPushUrl: true
+  };
+//will delay initialization of the SDK
+//make sure to call before init()
+  await OneSignal.shared
+      .init("cecb6df8-3335-4db1-a26b-ed4b7f1c9ae3", iOSSettings: settings);
+// the SDK will now initialize
+  await OneSignal.shared.consentGranted(true);
+//
+var status = await OneSignal.shared.getPermissionSubscriptionState();
+    print(status.subscriptionStatus.userId);
 
-  void onLocaleChange(Locale locale) {
-    setState(() {
-      _newLocaleDelegate = AppTranslationsDelegate(newLocale: locale);
-    });
-  }
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    // Create the router.
-    Router router = new Router();
-    // Define our Home page.
-    router.define('home', handler: new Handler(
-        handlerFunc: (BuildContext context, Map<String, dynamic> params) {
-      return new HomeScreen();
-    }));
-    // Define our Login page.
-    router.define('login', handler: new Handler(
-        handlerFunc: (BuildContext context, Map<String, dynamic> params) {
-      return new LoginScreen();
-    }));
-
-    // Define our Dormantory page.
-    router.define('dormitory', handler: new Handler(
-        handlerFunc: (BuildContext context, Map<String, dynamic> params) {
-      return new DormitoryListData();
-    }));
-
-    // Define our Dormantory page.
-    router.define('ferry', handler: new Handler(
-        handlerFunc: (BuildContext context, Map<String, dynamic> params) {
-      return new TransportListData();
-    }));
-
-    // Define our Student page.
-    router.define('student', handler: new Handler(
-        handlerFunc: (BuildContext context, Map<String, dynamic> params) {
-      String _parentId = params["parentId"]?.first;
-      String _screenType = params["screenType"]?.first;
-      String _userId = params["userId"]?.first;
-      return new StudentListData(
-          parentId: _parentId, screenType: _screenType, userId: _userId);
-    }));
-
-    // Define our Grade page.
-    router.define('grade', handler: new Handler(
-        handlerFunc: (BuildContext context, Map<String, dynamic> params) {
-      String _studentId = params["studentId"]?.first;
-      return new ExamGradeListData(
-        studentId: _studentId,
-      );
-    }));
-
-    // Define our Exam page.
-    router.define('exam', handler: new Handler(
-        handlerFunc: (BuildContext context, Map<String, dynamic> params) {
-      String _classId = params["classId"]?.first;
-      return new ExamScreen(
-        classId: _classId,
-      );
-    }));
-
-    // Define our Time Table page.
-    router.define('timetable', handler: new Handler(
-        handlerFunc: (BuildContext context, Map<String, dynamic> params) {
-      String _sectionId = params["sectionId"]?.first;
-      return new TimeTable(
-        sectionId: _sectionId,
-      );
-    }));
-
-    // Define our Time Table page.
-    router.define('leave', handler: new Handler(
-        handlerFunc: (BuildContext context, Map<String, dynamic> params) {
-      String _sid = params["studentId"]?.first;
-      String _puid = params["userId"]?.first;
-      String _sname = params["studentName"]?.first;
-      return new LeaveScreen(
-          userId: _puid, studentId: _sid, studentName: _sname);
-    }));
-    //Define our Setting Page.
-    router.define('setting', handler: new Handler(
-        handlerFunc: (BuildContext context, Map<String, dynamic> params) {
-      String _userid = params["userId"]?.first;
-      return new Setting(
-        userId: _userid,
-      );
-    }));
-
-    //Define our Notice Page.
-    router.define('notice', handler: new Handler(
-        handlerFunc: (BuildContext context, Map<String, dynamic> params) {
-      return new NoticeBoardScreen();
-    }));
-
-    //Define our Notice Page.
-    router.define('message', handler: new Handler(
-        handlerFunc: (BuildContext context, Map<String, dynamic> params) {
-      String _userid = params["userId"]?.first;
-      return new MessageScreen(userId: _userid);
-    }));
-
-    //Define our AboutSchool Page.
-    router.define('aboutSchool', handler: new Handler(
-        handlerFunc: (BuildContext context, Map<String, dynamic> params) {
-      return new AboutScreen();
-    }));
-
-    //Define our Student Detail Page.
-    router.define('studentDetail', handler: new Handler(
-        handlerFunc: (BuildContext context, Map<String, dynamic> params) {
-      String _sid = params["studentId"]?.first;
-      String _sname = params["studentName"]?.first;
-      return new StudentDetailScreen(
-        studentId: _sid,
-        studentName: _sname,
-      );
-    }));
-
-    // Defind Router
-    Application.router = router;
-
-    return new MaterialApp(
-      title: 'AVASMS',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(primarySwatch: Colors.deepPurple, fontFamily: 'Zawgyi'),
-      onGenerateRoute: Application.router.generator,
-      home: widget.loginStatus ? new HomeScreen() : new LoginScreen(),
-      localizationsDelegates: [
-        _newLocaleDelegate,
-        const AppTranslationsDelegate(),
-        //provides localised strings
-        GlobalMaterialLocalizations.delegate,
-        //provides RTL support
-        GlobalWidgetsLocalizations.delegate,
-      ],
-      supportedLocales: application.supportedLocales(),
-    );
-  }
-
-  Future<String> getLocalStorageData() async {
-    return await LocalStorage.get(Config.USER_RELATED_ID);
-  }
+      }
 }
