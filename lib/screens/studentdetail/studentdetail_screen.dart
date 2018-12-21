cc80c631f60fdb4752e63af1d7dfeed94013018d
avasms <avasmsteam@gmail.com>
2018-12-21 15:18:01 +0630

    phyo phyo

--- lib/screens/studentdetail/studentdetail_screen.dart
+++ lib/screens/studentdetail/studentdetail_screen.dart
@@ -12,20 +12,22 @@ import 'package:sms_parent/util/app_translation.dart';
 class StudentDetailScreen extends StatefulWidget {
   final studentId;
   const StudentDetailScreen({Key key, this.studentId}) : super(key: key);
-  
+
   //_AboutScreen createState() => new _AboutScreen();
-  _AboutStudent createState()=>new _AboutStudent();
+  _AboutStudent createState() => new _AboutStudent();
 }
 
 class _AboutStudent extends State<StudentDetailScreen> {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.indigo.shade700,
-      appBar:AppBar(title: new Text(
-            AppTranslations.of(context).text("detail"),
-            style: TextStyle(fontFamily: 'Myanmar', color: Colors.white),
-          )),
+      //backgroundColor: Colors.indigo.shade700,
+      appBar: AppBar(
+        backgroundColor: Colors.indigo.shade700,
+          title: new Text(
+        AppTranslations.of(context).text("detail"),
+        style: TextStyle(fontFamily: 'Myanmar', color: Colors.white),
+      )),
       body: FutureBuilder<StudentInfo>(
         future: new ApiCommonDao().viewStudentInfo(widget.studentId),
         builder: (context, snapshot) {
@@ -40,102 +42,105 @@ class _AboutStudent extends State<StudentDetailScreen> {
   }
 }
 
-class About extends StatefulWidget{
+class About extends StatefulWidget {
   final stud;
   About({this.stud});
-  _AboutScreen createState()=>new _AboutScreen();
+  _AboutScreen createState() => new _AboutScreen();
 }
-class _AboutScreen extends State<About> with SingleTickerProviderStateMixin{
-  TabController _controller;
-    @override
-    void initState() {
-      // TODO: implement initState
-      super.initState();
-      _controller=new TabController(length: 3,vsync: this);
 
-    }
-    @override
-      void dispose() {
-        // TODO: implement dispose
-        super.dispose();
-        _controller.dispose();
-      }
+class _AboutScreen extends State<About> with SingleTickerProviderStateMixin {
+  TabController _controller;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _controller = new TabController(length: 3, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _controller.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    
-    StudentInfo student=widget.stud; 
-    final studId=student.id;
+    StudentInfo student = widget.stud;
+    final studId = student.id;
     return new Scaffold(
-      
-      body:new Stack(
+      body: new Stack(
         children: <Widget>[
           new Column(
             children: <Widget>[
               new Expanded(
                 child: SingleChildScrollView(
                   child: new Column(
-        children: <Widget>[
-          new Stack(
-            alignment: const Alignment(0.0, 1.1),
-            children: <Widget>[
-              CircleAvatar(
-                backgroundImage: new NetworkImage(Config.BASE_URL +student.photoPath,),
-          radius: 60.0,
-        ),
-        
-            ],
-          ),
-          new Container(
-            alignment: const Alignment(0.0, 0.0),
-          decoration: BoxDecoration(
-            //color: Colors.white,
-          
-          ),
-          child: Text(
-            student.name,
-            style: TextStyle(
-              fontSize: 20.0,
-              fontWeight: FontWeight.bold,
-              fontFamily:'Zawgyi' ,
-              color: Colors.black,
-            ),
-          ),
-        ),
-          new Container(
-            decoration: new BoxDecoration(color: Theme.of(context).primaryColor),
-            child: new TabBar(
-            indicatorColor: Colors.redAccent,
-              controller: _controller,
-              tabs: [
-                new Tab(
-                  icon: const Icon(Icons.satellite,),
-                  
-                ),
-                /*new Tab(
+                    children: <Widget>[
+                      new Stack(
+                        alignment: const Alignment(0.0, 1.1),
+                        children: <Widget>[
+                          CircleAvatar(
+                            backgroundImage: new NetworkImage(
+                              Config.BASE_URL + student.photoPath,
+                            ),
+                            radius: 60.0,
+                          ),
+                        ],
+                      ),
+                      new Container(
+                        alignment: const Alignment(0.0, 0.0),
+                        decoration: BoxDecoration(
+                            //color: Colors.white,
+
+                            ),
+                        child: Text(
+                          student.name,
+                          style: TextStyle(
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: 'Zawgyi',
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+                      new Container(
+                        decoration: new BoxDecoration(
+                            color: Colors.white),
+                        child: new TabBar(
+                          //indicatorColor: Colors.redAccent,
+                          controller: _controller,
+                          tabs: [
+                            new Tab(
+                              /*icon: const Icon(
+                                Icons.satellite,
+                              ),*/
+                              text:student.name,
+                            ),
+                            /*new Tab(
                   icon: const Icon(Icons.schedule),
                 ),
                 new Tab(
                   icon: const Icon(Icons.payment),
                   //text: 'Address',
                 ),*/
-              ],
-            ),
-          ),
-          new Container(
-            height: MediaQuery.of(context).size.width,
-            //height:350.0,
-            child: new TabBarView(
-              controller: _controller,
-              children: <Widget>[
-                new InfoScreen(student: student),
-                //new AttendantScreen(studentId: studId),
-                //new PaymentScreen(studentId: studId,),
-              ],
-            ),
-          ),
-         
-        ],
-      ),
+                          ],
+                        ),
+                      ),
+                      new Container(
+                        height: MediaQuery.of(context).size.width,
+                        //height:350.0,
+                        child: new TabBarView(
+                          controller: _controller,
+                          children: <Widget>[
+                            new InfoScreen(student: student),
+                            //new AttendantScreen(studentId: studId),
+                            //new PaymentScreen(studentId: studId,),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ],
